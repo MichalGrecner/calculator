417c2a4dde240c03ec36c8e760677affa02135eb
Michal Grecner <grecnermichal@gmail.com>
2022-08-28 21:22:27 +0200

    gui changes when operators pushed + add +/-

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let memory=[]; //
-let num
+let num;
 let operation;
 let operationMemory;
+let canOperate = false;
+
 
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach(function (btn){
     btn.addEventListener("click", () => {
+        //resets CSS styles of operator buttons
+        buttons.forEach(function (btn){
+            if(btn.getAttribute("class").includes("operator")){
+            btn.setAttribute("style", "background-color:rgb(240, 160, 84)")
+            }
+        })
+        
+        console.log("can Operate: "+canOperate)
         if( btn.getAttribute("class").includes("num")){
+            canOperate = true;
             if(typeof num ==="undefined"){
                 num = String(btn.innerHTML)
                 display(num)
+                
             }
             else if (num.includes(".") &&  btn.getAttribute("class").includes("decSep")){
                 //pass
@@ -17,16 +29,28 @@ buttons.forEach(function (btn){
             else{
                 num += String(btn.innerHTML);
                 display(num)
+                
             }
             
-        }else if(btn.getAttribute("class").includes("operator")){
-            if( operation === undefined){
+        }else if(btn.getAttribute("class").includes("operator")&& canOperate === true){
+            canOperate = false;
+            if(btn.getAttribute("class").includes("plusMinus")){
+                num = Number(num) * -1
+                display(num)
+            }
+            if(btn.getAttribute("class").includes("equal")){
+                //manage when =
+            }
+
+            else if( operation === undefined){
                 operation = btn.innerHTML;
-                display(operation)
+                tagButton(btn) 
+            
             } else {
                 operationMemory = btn.innerHTML;
-                display(operationMemory)
+                tagButton(btn)
             }
+            
             memory.push(
                 Number(num));
             num = undefined;
@@ -34,7 +58,7 @@ buttons.forEach(function (btn){
                 console.log("count " + operation)
                 count()
             }
-        } else {
+        } else if (btn.getAttribute("class").includes("clear")){
             //CLEAR function
             memory = [];
             num = operation = operationMemory = undefined
@@ -42,10 +66,7 @@ buttons.forEach(function (btn){
         }
 
         
-        console.log("num: " + num);
-        console.log("memory: " + memory)
-        console.log("operation: " + operation)
-        console.log("operationMemory: " + operationMemory)
+      
     })
 }
 )
@@ -54,8 +75,9 @@ function count(){
 
     console.log("fce count - memory0: "+ memory[0])
     console.log("fce count - memory1: "+ memory[1])
-    console.log("fce count - memory0type of: "+ typeof memory[0])
-    console.log("fce count - memory1type of: "+ typeof memory[1])
+   
+    console.log("fce count - operation: " + operation)
+    console.log("fce count - operationMemory: " + operationMemory)
    
     const operations = {
         "+": memory[0] + memory[1],
@@ -71,17 +93,30 @@ function count(){
     memory[0]=(operations[operation])
     memory.pop()
     operation = operationMemory;
-   
-    
-
 }
+
+
+
+
+
 
 function display(value){
     let dispNum =  document.querySelector(".dispValue")
     dispNum.innerHTML = value;
 }
 
-//TODO: diplay graphic + max nm of digits on display, numbers with decimal point, allow only one decimal point
+function tagButton(btn){
+    btn.setAttribute("style", "background-color:white")
+    
+
+}
+
+//TODO: 
+// 1. When tapped 2x operation button
+// 2. Long decimal numbers precision
+// 3. graphics display 
+// 4. negative number
+
 
 
 
